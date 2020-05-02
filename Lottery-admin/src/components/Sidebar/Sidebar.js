2717b6c60240d3162fd660774aeb70256c3fe061
saadi-ninjasCode <saadjaved79@gmail.com>
2020-05-02 14:53:34 +0500

    sidebar formattting

--- Lottery-admin/src/components/Sidebar/Sidebar.js
+++ Lottery-admin/src/components/Sidebar/Sidebar.js
@@ -18,14 +18,12 @@
 /*eslint-disable*/
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-// nodejs library to set properties for components
-import { PropTypes } from "prop-types";
 
 // javascript plugin used to create scrollbars on windows
 import PerfectScrollbar from "perfect-scrollbar";
 
 // reactstrap components
-import { Nav, NavLink as ReactstrapNavLink } from "reactstrap";
+import { Nav } from "reactstrap";
 
 var ps;
 
@@ -55,6 +53,7 @@ class Sidebar extends React.Component {
     document.documentElement.classList.remove("nav-open");
   };
   render() {
+    console.log(this.props)
     const { bgColor, routes, rtlActive, logo } = this.props;
     let logoImg = null;
     let logoText = null;
@@ -137,47 +136,12 @@ class Sidebar extends React.Component {
                 </li>
               );
             })}
-            <li
-              className="active-pro"
-            >
-              <ReactstrapNavLink
-                href="https://www.creative-tim.com/product/black-dashboard-pro-react?ref=bdr-user-archive-sidebar-upgrade-pro"
-              >
-                <i className="tim-icons icon-spaceship" />
-                <p>Upgrade to PRO</p>
-              </ReactstrapNavLink>
-            </li>
+
           </Nav>
         </div>
       </div>
     );
   }
 }
-
-Sidebar.defaultProps = {
-  rtlActive: false,
-  bgColor: "primary",
-  routes: [{}]
-};
-
-Sidebar.propTypes = {
-  // if true, then instead of the routes[i].name, routes[i].rtlName will be rendered
-  // insde the links of this component
-  rtlActive: PropTypes.bool,
-  bgColor: PropTypes.oneOf(["primary", "blue", "green"]),
-  routes: PropTypes.arrayOf(PropTypes.object),
-  logo: PropTypes.shape({
-    // innerLink is for links that will direct the user within the app
-    // it will be rendered as <Link to="...">...</Link> tag
-    innerLink: PropTypes.string,
-    // outterLink is for links that will direct the user outside the app
-    // it will be rendered as simple <a href="...">...</a> tag
-    outterLink: PropTypes.string,
-    // the text of the logo
-    text: PropTypes.node,
-    // the image src of the logo
-    imgSrc: PropTypes.string
-  })
-};
 
 export default Sidebar;
