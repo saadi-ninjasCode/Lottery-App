62f667bb048cbbf6880bbdb4974f71be6cf482c0
saadi-ninjasCode <saadjaved79@gmail.com>
2020-05-18 21:48:05 +0500

    graphql schemas for users

--- Lottery-api/graphql/schema/index.js
+++ Lottery-api/graphql/schema/index.js
@@ -2,22 +2,38 @@ const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
 type LotteryType{
-    id:ID!
-    name:String!
+    _id: ID!
+    name: String!
     next_draw: String
+}
+
+type User{
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    notifications: [LotteryType!]
 }
 
 input LotteryInput {
-    name:String!
+    name: String!
     next_draw: String
 }
 
+input UserInput{
+    name: String!
+    email: String!
+    password: String!
+}
+
 type RootQuery {
-    lottery(id:Int): [LotteryType!]!
+    lottery(id : String) : [LotteryType!]!
+    user(id : String) : [User!]!
 }
 
 type RootMutation{
-    createLottery(lotteryInput:[LotteryInput]):[LotteryType!]
+    createLottery( lotteryInput : LotteryInput ) : LotteryType!
+    createUser( userInput : UserInput ) : User!
 }
 
 schema {
