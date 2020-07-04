485fbee0ee6f3e47fb974e431cefa976c9e2b276
saadi-ninjasCode <saadjaved79@gmail.com>
2020-07-04 19:39:29 +0500

    route accress accorgin to login or not

--- Lottery-admin/src/App.js
+++ Lottery-admin/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import AdminLayout from "layouts/Admin.js";
-import Auth from "layouts/Auth";
+import Auth from "./layouts/Auth";
+import PrivateRotue from './views/PrivateRoute'
 
 
 function APP() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/admin" render={props => <AdminLayout {...props} />} />
+                <PrivateRotue path="/admin"
+                    component={props => <AdminLayout {...props} />} />
                 <Route path='/auth' component={props => <Auth {...props} />} />
                 <Redirect from="/" to="/admin/dashboard" />
             </Switch>
