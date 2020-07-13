15cbdfd52f8bae23fe8a432fe584bafe73359054
saadi-ninjasCode <saadjaved79@gmail.com>
2020-07-14 01:07:17 +0500

    dashboard -> update routes

--- Lottery-admin/src/routes.js
+++ Lottery-admin/src/routes.js
@@ -17,11 +17,6 @@
 */
 import Dashboard from "views/Dashboard.js";
 import Icons from "views/Icons.js";
-import Map from "views/Map.js";
-import Notifications from "views/Notifications.js";
-import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import UserProfile from "views/UserProfile.js";
 import LunchTime from "views/LunchTime.js"
 import LotteryBalls from "./views/LotteryBalls";
 import Login from './views/Login'
@@ -44,46 +39,6 @@ var routes = [
     icon: "tim-icons icon-atom",
     component: Icons,
     layout: "/admin"
-  },
-  {
-    path: "/map",
-    name: "Map",
-    icon: "tim-icons icon-pin",
-    component: Map,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: "/admin"
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: "/admin"
-  },
-  {
-    path: "/tables",
-    name: "Table List",
-    icon: "tim-icons icon-puzzle-10",
-    component: TableList,
-    layout: "/admin"
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "tim-icons icon-align-center",
-    component: Typography,
-    layout: "/admin"
-  },
-  {
-    path: "/rtl-support",
-    name: "RTL Support",
-    icon: "tim-icons icon-world",
   },
   {
     path: "/LunchTime",
