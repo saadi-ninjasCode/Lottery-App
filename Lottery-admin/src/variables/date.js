178be9304bb82889a5433a3215c98f4485d5652d
saadi-ninjasCode <saadjaved79@gmail.com>
2020-10-28 12:18:34 +0500

    update date file

--- Lottery-admin/src/variables/date.js
+++ Lottery-admin/src/variables/date.js
@@ -1,7 +1,23 @@
-exports.dateToString = date => new Date(date).toISOString();
-exports.dateToLocal = date => new Date(date).toLocaleString();
-exports.dateTolocalDate = date => new Date(date).toLocaleDateString();
-exports.dateToCustom = date => new Date(+date).toLocaleString(undefined, { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
-exports.dateWithWeekday = date => new Date(+date).toLocaleDateString(undefined, { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'short' })
+import moment from 'moment-timezone'
+
+function dateToString(date) { return new Date(date).toISOString() }
+function dateToLocal(date) { return new Date(date).toLocaleString() }
+function dateTolocalDate(date) { return new Date(date).toLocaleDateString() }
+function dateToCustom(date) { return new Date(+date).toLocaleString(undefined, { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }) }
+function dateWithWeekday(date) { return new Date(+date).toLocaleDateString(undefined, { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'short' }) }
+function dateToCustomLondon(date) { return new Date(+date).toLocaleString('en', { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZone: 'Europe/London' }) }
+function dateWithWeekdayLondon(date) { return new Date(+date).toLocaleDateString('en', { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZone: 'Europe/London', timeZoneName: 'short' }) }
+
+function dateToCustomMoment(date) {
+    return moment(+date).format("DD MMM YYYY, hh:mm A")
+}
+function dateWithWeekdayMoment(date) {
+    return moment(+date).format("LLLL")
+}
+
+
+export { dateToString, dateTolocalDate, dateToLocal, dateToCustom, dateToCustomLondon, dateToCustomMoment, dateWithWeekday, dateWithWeekdayLondon, dateWithWeekdayMoment }
 
 // new Date(date).toLocaleString('en-US', { timezone: 'Asia/Karachi' })
+
+// new Date().toLocaleString('en', { timeZone: 'Europe/London' })
