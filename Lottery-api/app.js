3f9d4075bbda8e6c0ab01e8f473759748b9c68ae
Saadi <saad@Ms-MacBook-Pro.local>
2021-05-15 21:43:02 +0500

    Dashboard build

--- Lottery-api/app.js
+++ Lottery-api/app.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const { ApolloServer, gql } = require('apollo-server-express')
-const isAuth = require('./middleware/is-auth')
-const http = require('http')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { ApolloServer, gql } = require("apollo-server-express");
+const isAuth = require("./middleware/is-auth");
+const http = require("http");
 //Configure the dotenv for loading the environment variable from .env file to process.env
-dotenv.config()
+dotenv.config();
 
-const graphQlSchema = require('./graphql/schema/index')
-const graphQlResolvers = require('./graphql/resolvers/index');
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -17,33 +17,35 @@ app.use(bodyParser.json());
 
 app.use(isAuth);
 
-const server = new ApolloServer({
-    typeDefs: graphQlSchema,
-    resolvers: graphQlResolvers,
-    context: ({ req, res }) => {
-        // const isAuth = req.isAuth
-        // const userId = req.userId
-        return { req, res}
-    }
-});
+app.use("/dashboard", express.static(path.join(__dirname, "/build")));
 
+const server = new ApolloServer({
+  typeDefs: graphQlSchema,
+  resolvers: graphQlResolvers,
+  context: ({ req, res }) => {
+    // const isAuth = req.isAuth
+    // const userId = req.userId
+    return { req, res };
+  },
+});
 
 server.applyMiddleware({ app });
 const httpServer = http.createServer(app);
-server.installSubscriptionHandlers(httpServer)
+server.installSubscriptionHandlers(httpServer);
 
-app.get('/', function (req, res) {
-    res.send("Ready to Display")
-})
+app.get("/", function (req, res) {
+  res.send("Ready to Display");
+});
 
-mongoose.connect(process.env.CONNECTION_STRING,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        httpServer.listen(process.env.PORT, () => {
-            console.log(`🚀  server started at http://localhost:${process.env.PORT}${server.graphqlPath}`)
-            console.log(`🚀 Subscriptions ready at ws://localhost:${process.env.PORT}${server.subscriptionsPath}`)
-        })
-    })
-    .catch(err => console.log("DB Connection Error : \n ", err))
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    httpServer.listen(process.env.PORT, () => {
+      console.log(`🚀  server started at http://localhost:${process.env.PORT}${server.graphqlPath}`);
+      console.log(`🚀 Subscriptions ready at ws://localhost:${process.env.PORT}${server.subscriptionsPath}`);
+    });
+  })
+  .catch((err) => console.log("DB Connection Error : \n ", err));
---
+++ Lottery-api/build/precache-manifest.f1d520e0abfabbeaff99e0457b262879.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189ea683713444947ae67172f76fe971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382c304323ffd711042b",
+    "url": "/static/css/2.e94d5e4c.chunk.css"
+  },
+  {
+    "revision": "a7e38c95b0322a1bce2a",
+    "url": "/static/css/main.b6204a32.chunk.css"
+  },
+  {
+    "revision": "382c304323ffd711042b",
+    "url": "/static/js/2.80cd26a5.chunk.js"
+  },
+  {
+    "revision": "c65c2ac2ed79c43a30e9165117fdb4aa",
+    "url": "/static/js/2.80cd26a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e38c95b0322a1bce2a",
+    "url": "/static/js/main.1c1e6681.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/static/js/main.1c1e6681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6628a4bc8cada34f4abd",
+    "url": "/static/js/runtime-main.996cfed1.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "64d1d241435a7fe08eecb92dee64c13e",
+    "url": "/static/media/lottery-logo.64d1d241.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
